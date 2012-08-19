9806aecaf63c9fc0aa79377761caa025804fe066
nt67 <n.takeda67@gmail.com>
2012-08-19 11:06:24 +0900

    create build file

---
+++ src/FxSample.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.state.Stage;
+
+public class FxSample extends Application {
+    public static void main(String[] args){
+	launch(args);
+	launch(FxSample.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+    }
+}
