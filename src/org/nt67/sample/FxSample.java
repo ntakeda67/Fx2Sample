641fbfb4d05745a1ed5c60cf4508333b7f03a0ee
nt67 <n.takeda67@gmail.com>
2012-08-19 14:08:13 +0900

    HelloWorld実装

--- src/org/nt67/sample/FxSample.java
+++ src/org/nt67/sample/FxSample.java
@@ -2,6 +2,10 @@ package org.nt67.sample;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
 
 public class FxSample extends Application {
     public static void main(String[] args){
@@ -11,5 +15,18 @@ public class FxSample extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
+	// set window title.
+	stage.setTitle("Hello world");
+	
+	// constrcut element.
+	Pane rootPane = new Pane();
+	Scene scene = new Scene(rootPane, 256, 212);
+	Label label = new Label("Hello World!");
+
+	// constructed element add to the tree of pane.
+	rootPane.getChildren().add(label);
+
+	stage.setScene(scene);
+	stage.show();
     }
 }
