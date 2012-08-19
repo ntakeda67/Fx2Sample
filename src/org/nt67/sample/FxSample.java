b20818099fe9f266752c501e456ef40c0ee66020
nt67 <n.takeda67@gmail.com>
2012-08-19 13:40:33 +0900

    antタスク追加。clean(class削除), batch(実効)

---
+++ src/org/nt67/sample/FxSample.java
@@ -0,0 +1,15 @@
+package org.nt67.sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxSample extends Application {
+    public static void main(String[] args){
+	launch(args);
+	launch(FxSample.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+    }
+}
